b874b5f99466d09d60fbc4c578fa5922600b57c8
heiljj <heiljj@rose-hulman.edu>
2026-02-10 21:48:32 -0600

    removed numpy from example

--- example.py
+++ example.py
@@ -1,12 +1,11 @@
 import time
 import shutil
-import numpy as np
 from ascutil import mutate
 
 shutil.copy("circuit.asc", "circuit_copy.asc")
 
-rows = np.array(range(16))
-columns = np.array(range(54))
+rows = list(range(16))
+columns = list(range(54))
 
 t1 = time.time()
 mutate("circuit_copy.asc", rows, columns, 0.5)
